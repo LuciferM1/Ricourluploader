d6a4dd7f35b9d63e872cc5ac6524f6ee71035350
LuciferM1 <87906594+LuciferM1@users.noreply.github.com>
2021-10-07 16:59:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1918817506:AAGHDwJ40y2mJtqGWzzYCBTUMNihYnILukU")
-    APP_ID = int(os.environ.get("APP_ID", 7139560))
-    API_HASH = os.environ.get("API_HASH", "805d46ae2d01bf1591bea89f54e76f13")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1269408652 1886319600").split())
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    APP_ID = int(os.environ.get("APP_ID", ))
+    API_HASH = os.environ.get("API_HASH", "3")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
